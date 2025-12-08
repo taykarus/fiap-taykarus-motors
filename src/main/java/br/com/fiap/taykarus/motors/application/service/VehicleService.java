4caf592cbfdbd74916a96ecd2b434e5929ffda35
Alison Israel <taykarus@gmail.com>
2025-12-08 18:57:32 -0300

    feat: add VehicleService

---
+++ src/main/java/br/com/fiap/taykarus/motors/application/service/VehicleService.java
@@ -0,0 +1,96 @@
+package br.com.fiap.taykarus.motors.application.service;
+
+import br.com.fiap.taykarus.motors.application.ports.in.ManageVehicleUseCase;
+import br.com.fiap.taykarus.motors.application.ports.in.dto.EditVehicleCommand;
+import br.com.fiap.taykarus.motors.application.ports.in.dto.RegisterVehicleCommand;
+import br.com.fiap.taykarus.motors.application.ports.in.dto.VehicleDTO;
+import br.com.fiap.taykarus.motors.application.ports.out.VehicleRepository;
+import br.com.fiap.taykarus.motors.domain.common.Price;
+import br.com.fiap.taykarus.motors.domain.vehicle.Color;
+import br.com.fiap.taykarus.motors.domain.vehicle.ModelYear;
+import br.com.fiap.taykarus.motors.domain.vehicle.Vehicle;
+import br.com.fiap.taykarus.motors.domain.vehicle.VehicleStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class VehicleService implements ManageVehicleUseCase {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    @Override
+    public VehicleDTO register(RegisterVehicleCommand command) {
+        Vehicle newVehicle = new Vehicle(
+                command.brand(),
+                command.model(),
+                new ModelYear(command.year()),
+                new Color(command.color()),
+                new Price(command.price())
+        );
+
+        Vehicle saved = vehicleRepository.save(newVehicle);
+
+        return mapToDTO(saved);
+    }
+
+    @Override
+    public VehicleDTO edit(EditVehicleCommand command) {
+        Vehicle vehicle = vehicleRepository.findById(command.id())
+                .orElseThrow(() -> new IllegalArgumentException("Vehicle not found"));
+
+        if (command.brand() != null) vehicle.updateBrand(command.brand());
+        if (command.model() != null) vehicle.updateModel(command.model());
+        if (command.year() != null) vehicle.updateYear(new ModelYear(command.year()));
+        if (command.color() != null) vehicle.updateColor(new Color(command.color()));
+        if (command.price() != null) vehicle.updatePrice(new Price(command.price()));
+
+        Vehicle saved = vehicleRepository.save(vehicle);
+
+        return mapToDTO(saved);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        if (vehicleRepository.findById(id).isEmpty()) {
+            throw new IllegalArgumentException("Vehicle not found");
+        }
+        vehicleRepository.delete(id);
+    }
+
+    @Override
+    public List<VehicleDTO> listVehiclesForSale() {
+        return vehicleRepository.findAllByStatusOrderByPriceAsc(VehicleStatus.FOR_SALE)
+                .stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<VehicleDTO> listSoldVehicles() {
+        return vehicleRepository.findAllByStatusOrderByPriceAsc(VehicleStatus.SOLD)
+                .stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private VehicleDTO mapToDTO(Vehicle v) {
+        LocalDateTime created = v.getHistory().isEmpty() ? LocalDateTime.now() : v.getHistory().getFirst().timestamp();
+
+        return new VehicleDTO(
+                v.getId(),
+                v.getBrand(),
+                v.getModel(),
+                v.getYear().value(),
+                v.getColor().value(),
+                v.getPrice().value(),
+                v.getStatus().name(),
+                created
+        );
+    }
+}
