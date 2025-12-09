df9cae8440491a089e853e4130aacbe9f645cede
Alison Israel <taykarus@gmail.com>
2025-12-08 23:09:16 -0300

    chore: fix wrong tab in Application file

--- src/main/java/br/com/fiap/taykarus/motors/infrastructure/MotorsApplication.java
+++ src/main/java/br/com/fiap/taykarus/motors/infrastructure/MotorsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    MotorsApplication {
+public class MotorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MotorsApplication.class, args);
