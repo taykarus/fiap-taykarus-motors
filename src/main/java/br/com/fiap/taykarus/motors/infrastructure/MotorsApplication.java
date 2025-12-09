bf507d4fb312dae10a9120e31e7bd2b1fdd6f9d0
Alison Israel <taykarus@gmail.com>
2025-12-08 23:06:30 -0300

    config: add infrastructure package and add DomainConfig to Spring

--- src/main/java/br/com/fiap/taykarus/motors/infrastructure/MotorsApplication.java
+++ src/main/java/br/com/fiap/taykarus/motors/infrastructure/MotorsApplication.java
@@ -1,10 +1,10 @@
-package br.com.fiap.taykarus.motors;
+package br.com.fiap.taykarus.motors.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MotorsApplication {
+public class    MotorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MotorsApplication.class, args);
